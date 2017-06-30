04cb28f560a9fefbbe5f849ceb69d371d081a22e
Lucas Cardoso <thelucascardoso@gmail.com>
2017-06-30 12:48:50 -0300

    Relógio e Tela funcionais

---
+++ src/util/Relogio.java
@@ -0,0 +1,73 @@
+package util;
+
+import javax.swing.JLabel;
+
+public class Relogio extends Thread {
+
+	private int hora, min, seg;
+	private JLabel lbl;
+	private long millis;
+	private String rel;
+
+	public Relogio(JLabel label) {
+		hora = 00;
+		min = 00;
+		seg = 00;
+		lbl = label;
+		millis = 1000;
+		rel = hora+":"+min+":"+seg;
+		lbl.setText(rel);
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				seg += 1;
+				Thread.sleep(millis);
+				rel = hora+":"+min+":"+seg;
+				lbl.setText(rel);
+				System.out.println("Drift na contagem - "+ millis);
+				if (seg > 60) {
+					min += 1;
+					seg = 0;
+					rel = hora+":"+min+":"+seg;
+					lbl.setText(rel);
+				}else if (min > 60) {
+					hora += 1;
+					min = 0;
+					rel = hora+":"+min+":"+seg;
+					lbl.setText(rel);
+				} else if (hora > 23) {
+					hora = 0;
+					rel = hora+":"+min+":"+seg;
+					lbl.setText(rel);
+				}
+			} catch (InterruptedException e) {
+				System.out.println("Erro no delay do contador");
+			}
+		}
+	}
+
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+
+	public void setMin(int min) {
+		this.min = min;
+	}
+
+	public void setSeg(int seg) {
+		this.seg = seg;
+	}
+
+	public void setMillis(long millis) {
+		this.millis = millis;
+	}
+	
+	public void refresh(){
+		rel = hora+":"+min+":"+seg;
+		lbl.setText(rel);
+	}
+	
+}
---
+++ src/view/Tela.java
@@ -0,0 +1,183 @@
+package view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+import util.Relogio;
+
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Tela {
+
+	private JFrame frame;
+	private JTextField hr, min, seg, drift;
+	private Relogio rel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Tela window = new Tela();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Tela() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+
+		frame = new JFrame();
+		frame.setBounds(100, 100, 489, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+
+		JLabel relogio = new JLabel("New label");
+		relogio.setHorizontalAlignment(SwingConstants.CENTER);
+		relogio.setFont(new Font("Tahoma", Font.PLAIN, 62));
+		relogio.setBounds(67, 24, 366, 75);
+		rel = new Relogio(relogio);
+		frame.getContentPane().add(relogio);
+
+		hr = new JTextField();
+		hr.setBounds(53, 110, 89, 20);
+		frame.getContentPane().add(hr);
+		hr.setColumns(10);
+
+		min = new JTextField();
+		min.setBounds(211, 110, 89, 20);
+		frame.getContentPane().add(min);
+		min.setColumns(10);
+
+		seg = new JTextField();
+		seg.setBounds(380, 110, 86, 20);
+		frame.getContentPane().add(seg);
+		seg.setColumns(10);
+
+		ButtonHandller btmH = new ButtonHandller();
+
+		JButton btnStart = new JButton("Start");
+		btnStart.addActionListener(btmH);
+		btnStart.setBounds(53, 227, 89, 23);
+		frame.getContentPane().add(btnStart);
+
+		JButton btnStop = new JButton("Stop");
+		btnStop.addActionListener(btmH);
+		btnStop.setBounds(344, 227, 89, 23);
+		frame.getContentPane().add(btnStop);
+
+		JLabel lblHora = new JLabel("Hora - ");
+		lblHora.setBounds(10, 113, 46, 14);
+		frame.getContentPane().add(lblHora);
+
+		JLabel lblMinuto = new JLabel("Minuto - ");
+		lblMinuto.setBounds(152, 113, 52, 14);
+		frame.getContentPane().add(lblMinuto);
+
+		JLabel lblSegundo = new JLabel("Segundo - ");
+		lblSegundo.setBounds(310, 113, 60, 14);
+		frame.getContentPane().add(lblSegundo);
+
+		drift = new JTextField();
+		drift.setBounds(175, 184, 86, 20);
+		frame.getContentPane().add(drift);
+		drift.setColumns(10);
+
+		JLabel lblDtift = new JLabel("Dtift -");
+		lblDtift.setBounds(96, 187, 46, 14);
+		frame.getContentPane().add(lblDtift);
+
+		JButton btnSetDrift = new JButton("Set Drift");
+		btnSetDrift.addActionListener(btmH);
+		btnSetDrift.setBounds(293, 178, 89, 23);
+		frame.getContentPane().add(btnSetDrift);
+
+		JButton btnSetHora = new JButton("Set Hora");
+		btnSetHora.addActionListener(btmH);
+		btnSetHora.setBounds(53, 133, 89, 23);
+		frame.getContentPane().add(btnSetHora);
+
+		JButton btnSetMinuto = new JButton("Set Min");
+		btnSetMinuto.addActionListener(btmH);
+		btnSetMinuto.setBounds(211, 133, 89, 23);
+		frame.getContentPane().add(btnSetMinuto);
+
+		JButton btnSetSegundo = new JButton("Set Seg");
+		btnSetSegundo.addActionListener(btmH);
+		btnSetSegundo.setBounds(377, 133, 89, 23);
+		frame.getContentPane().add(btnSetSegundo);
+	}
+
+	private class ButtonHandller implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if ("Start".equals(e.getActionCommand())) {
+				rel.start();
+			} else if ("Stop".equals(e.getActionCommand())) {
+				
+			} else if ("Set Drift".equals(e.getActionCommand())) {
+				if ("".equals(drift.getText())) {
+					JOptionPane.showMessageDialog(null, "Insira um valor de Drift!!");
+				} else {
+					long aux = Long.parseLong(drift.getText());
+					rel.setMillis(aux);					
+				}
+			} else if ("Set Hora".equals(e.getActionCommand())) {
+				if ("".equals(hr.getText())){
+					JOptionPane.showMessageDialog(null, "Insira um valor da Hora!!");
+				} else {
+					int aux = Integer.parseInt(hr.getText());
+					rel.setHora(aux);
+					rel.refresh();
+				}
+			} else if ("Set Min".equals(e.getActionCommand())) {
+				if ("".equals(min.getText())) {
+					JOptionPane.showMessageDialog(null, "Insira um valor de Minutos!!");
+				} else {
+					int aux = Integer.parseInt(min.getText());
+					rel.setMin(aux);
+					rel.refresh();
+				}
+
+			} else if ("Set Seg".equals(e.getActionCommand())) {
+				if ("".equals(seg.getText())) {
+					JOptionPane.showMessageDialog(null, "Insira um valor de Segundos!!");
+				} else {
+					int aux = Integer.parseInt(seg.getText());
+					rel.setSeg(aux);
+					rel.refresh();
+				}
+			}
+
+		}
+
+	}
+
+	private void main() {
+		// TODO Auto-generated method stub
+		Tela t = new Tela();
+	}
+}
